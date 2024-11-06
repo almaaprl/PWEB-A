a82f932827bb623c357e4b843ed54af09d98badf
almaaprl <162028579+almaaprl@users.noreply.github.com>
2024-11-06 19:56:09 +0700

    Create regist.php

---
+++ ETS/WebGabung/regist.php
@@ -0,0 +1,15 @@
+<?php
+require 'koneksi.php';
+$fullname = $_POST["fullname"];
+$username = $_POST["username"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$query_sql = "INSERT INTO users (fullname, username, email, password) 
+            VALUES ('$fullname', '$username', '$email', '$password')";
+
+if (mysqli_query($conn, $query_sql)) {
+    header("Location: index.html");
+} else {
+    echo "Pendaftaran Gagal : " . mysqli_error($conn);
+}
