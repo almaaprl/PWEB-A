b08c192333d7ac6e2d936d27a24068c9b01ff43d
almaaprl <162028579+almaaprl@users.noreply.github.com>
2024-11-06 19:57:20 +0700

    Update index.php

--- ETS/WebGabung/WebCRUD/index.php
+++ ETS/WebGabung/WebCRUD/index.php
@@ -1,1 +1,11 @@
+<?php
+require_once 'navbar.php';
+echo "<link rel='stylesheet' href='style.css'>";
 
+?>
+
+<div class="home" id="home">
+    <h1 class="welcome1">Hello, Welcome to</h1>
+    <h1 class="welcome2">Sistem Manajemen Data Pegawai (SMDP)</h1>
+    <a class="scroll" href="read.php">Let's Check!</a>
+</div>
